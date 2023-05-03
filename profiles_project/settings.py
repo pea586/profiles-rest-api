2328b8d355115bab88b27c36bdc60f168f2bfcae
Bill <keepgoingbill@gmail.com>
2023-05-03 23:38:00 +0800

    Configured for deploym t exit

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+$+$0of%#3ku-4abp^2(^!#hm@%8)7k#p-zl%j4g13o^o^ups%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
